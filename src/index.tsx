a28b815eca6e85a413a4d09a9c675287da3bceb1
Vladislav.Kubasov <kubasovvv07@gmail.com>
2022-08-14 20:58:32 +0400

    added TS

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootEl);
 
 root.render(<App />);
